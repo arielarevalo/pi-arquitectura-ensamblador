c56ca8af8d6b52d20c6d091643cc99a21769f9b3
yenjimque <yjimenezquesada@gmail.com>
2022-06-14 00:30:53 -0600

    Se agrega el codigo para el controlador de botones y se deja funcionando proyecto de QT

--- Semaforo/Main.cpp
+++ Semaforo/Main.cpp
@@ -44,9 +44,37 @@ void coordinar() {
 
 void entrada(int pos) {
 	BOTON[pos] = 1;
+	int indice = 0;
 
 	__asm {
 		// CONTR controlador(BOTON)
+		LEA EDI, BOTON            // Lee el array de botones
+		LEA ESI, CONTR            // Lee el array para el controlador
+
+		conBotones: CMP indice, 3 //Compara el indice a comparar
+		            JG FIN        // Si es mayor que 3 da Fin
+		            JMP COMPARA   // Si no, lo manda a COMPARA
+
+		COMPARA: MOV AL, [EDI]    // Lee el dato del array de botones
+		         CMP AL, 1        // Compara el dato leido contra un 1
+		         JE ASIGNA        // Si son iguales llama a ASIGNA 
+		         JMP AVANZA       // Si son distintos llama a AVANZA
+
+		ASIGNA: MOV [ESI], 1      // Asigna como 1 el valor del array de control
+		        MOV [EDI], 0      // Limpia la posicion del array de botones
+			    INC EDI           // Avanza en el array de botones
+			    INC ESI           // Avanza en el array de control
+			    INC indice        // Aumenta el indice
+				JMP conBotones    // Se devuelve al controlador
+
+		AVANZA: MOV[ESI], 0       // Limpia la posicion en el array de control
+				MOV[EDI], 0       // Limpia la posicion en el array de botones
+				INC EDI           // Avanza en el array de botones
+				INC ESI           // Avanza en el array de control
+			    INC indice        // Aumenta el indice
+				JMP conBotones    // Se devuelve al controlador
+
+		FIN: MOV indice, 0        // Limpia el indice para evitar basura
 	
 		// VAL_I codificador(CONTR)
 
