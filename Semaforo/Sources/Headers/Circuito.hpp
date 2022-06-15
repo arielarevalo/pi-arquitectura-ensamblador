85931e776901f746519ad1aba257bb69472aa0ba
yenjimque <yjimenezquesada@gmail.com>
2022-06-15 02:40:40 -0600

    Se cambia el codigo de ensamblador del controlador de botones a la nueva estrut¡ctura y se

--- Semaforo/Sources/Headers/Circuito.hpp
+++ Semaforo/Sources/Headers/Circuito.hpp
@@ -88,8 +88,39 @@ void Circuito::salida() {
 
 void Circuito::controlar() {
 	Logger::info("Controlando botones. Valor de entrada: " + imprimirArreglo(BOTON, TAM_BOTON));
+	int indice = 0;
 	__asm {
 		// CONTR controlador(BOTON)
+		
+		LEA EDI, BOTON             // Lee el array de botones
+		LEA ESI, CONTR             // Lee el array para el controlador
+
+		conBotones : CMP indice, 3 //Compara el indice a comparar
+		JG FIN                     // Si es mayor que 3 da Fin
+		JMP COMPARA                // Si no, lo manda a COMPARA
+
+		COMPARA : MOV AL, [EDI]    // Lee el dato del array de botones
+		CMP AL, 1                  // Compara el dato leido contra un 1
+		JE ASIGNA                  // Si son iguales llama a ASIGNA 
+		JMP AVANZA                 // Si son distintos llama a AVANZA
+
+		ASIGNA : MOV[ESI], 1       // Asigna como 1 el valor del array de control
+		MOV[EDI], 0                // Limpia la posicion del array de botones
+		INC EDI                    // Avanza en el array de botones
+		INC ESI                    // Avanza en el array de control
+		INC indice                 // Aumenta el indice
+		JMP conBotones             // Se devuelve al controlador
+
+		AVANZA : MOV[ESI], 0       // Limpia la posicion en el array de control
+		MOV[EDI], 0                // Limpia la posicion en el array de botones
+		INC EDI                    // Avanza en el array de botones
+		INC ESI                    // Avanza en el array de control
+		INC indice                 // Aumenta el indice
+		JMP conBotones             // Se devuelve al controlador
+
+		FIN : MOV indice, 0        // Limpia el indice para evitar basura
+
+		// Fin CONTR controlador(BOTON)
 	}
 	Logger::info("Control de botones termina. Valor de salida: " + imprimirArreglo(CONTR, TAM_CONTR));
 }
